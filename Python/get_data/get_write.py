f3387011cdd6753c1b5e6bfcc26f988344905e08
KimitoTetsu <80682435+KimitoTetsu@users.noreply.github.com>
2023-10-09 18:21:55 +0300

    Update get_write.py
    added comments

--- Python/get_data/get_write.py
+++ Python/get_data/get_write.py
@@ -2,19 +2,20 @@ import websocket
 import math
 import os
 import json
-
+#operation on a received message to write to a file
 def on_message(ws, msg):
     print(msg)
     resp = json.loads(msg)
     value = (resp["id"]) 
-    folder = math.floor(value/100)
+    folder = math.floor(value/100)# the number of messages we write in one folder. if (value/100) then 100 pieces each
     os.system("mkdir /home/jupyter/data/"+str(folder))
-    with open("/home/jupyter/data/"+str(folder)+"/"+str(value)+".json", 'w') as datafile:
-        datafile.write(msg)
-        datafile.close
-    with open("/home/jupyter/logs/readlog.txt", 'a') as logfile:
-        logfile.write(str(value)+"\n")
-        logfile.close
+    datafile = open("/home/jupyter/data/"+str(folder)+"/"+str(value)+".json", 'w')
+    datafile.write(msg)
+    datafile.close
+    #record the recorded message in the log
+    logfile = open("/home/jupyter/logs/readlog.txt", 'a')
+    logfile.write(str(value)+"\n")
+    logfile.close
 if __name__ == "__main__":
     ws = websocket.WebSocketApp("wss://test-ws.skns.dev/raw-messages", on_message=on_message)
     ws.run_forever()
